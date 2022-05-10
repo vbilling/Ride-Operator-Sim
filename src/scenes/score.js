904dda80c8e01d93a6e13ecf3f5f8f2d00a588dc
Victoria Billings <vbilling@ucsc.edu>
2022-05-09 23:24:26 -0700

    characters drawn on score page

--- src/scenes/score.js
+++ src/scenes/score.js
@@ -15,8 +15,15 @@ class score extends Phaser.Scene{
         for(let i = 0; i < (customers); i++){
             console.log('i:', i);
             
-            this.add.text(100 + this.textDistance, 200, allRiders_array[i]);
+            this.info = this.add.text(100 + this.textDistance, 200, allRiders_array[i]);
             this.textDistance += 150;
+            this.customer = this.add.sprite(this.info.x + 60, this.info.y + 350, allRiders_array[i][0]);
+            this.customer.setScale(0.2);
+            //now add the accessories
+            for(let a = 0; a < allRiders_array[i].length; a++){
+                this.accessory = this.add.sprite(this.customer.x, this.customer.y, allRiders_array[i][a]);
+                this.accessory.setScale(0.2);
+            }
         }
 
 
